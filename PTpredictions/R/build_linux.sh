e4e5fd76c2a8b0851cb31d51650c812a1e47281c
Hatef Monajemi <monajemi@stanford.edu>
2018-08-01 14:48:00 -0700

    name change

--- PTpredictions/R/build_linux.sh
+++
@@ -1,2 +0,0 @@
-R CMD BUILD PT
-R CMD INSTALL -l /home/hatef/R/i686-pc-linux-gnu-library/2.15/ PT
--- PTpredictions/R/build_mac.sh
+++
@@ -1,3 +0,0 @@
-R CMD BUILD PT
-#R CMD INSTALL -l /Library/Frameworks/R.framework/Versions/3.0/Resources/library/ PT
-R CMD INSTALL -l /Library/Frameworks/R.framework/Versions/Current/Resources/library PT
